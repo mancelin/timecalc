3cc77c8feeccc717c55221b9b2f8427364a979fc
Maxime Ancelin <ancelin.maxime@gmail.com>
2019-01-27 21:05:17 +0100

    Check if timesting is valid

--- timecalc.py
+++ timecalc.py
@@ -1,9 +1,10 @@
+import re
 import sys
 
 class Time:
-  def __init__(self, time_string):
-    print("time_string : " + time_string)
-    time_list = time_string.split('h')
+  def __init__(self, timestring):
+    print("timestring : " + timestring)
+    time_list = timestring.split('h')
     # just minutes, ex : '30'
     if len(time_list) == 1:
       self.hours = 0
@@ -19,9 +20,23 @@ class Time:
     return "{} hours {} minutes".format(self.hours, self.minutes)
 
 
+def is_timestring(str):
+  return re.match('^\d*h?\d*?$', str) != None
+
+
+def test_is_timestring(str):
+  print("is_timestring('{}') : {}".format(str, is_timestring(str)))
+
+
 if __name__ == '__main__':
   print("timecalc")
   args = sys.argv[1:]
   print(args)
   t = Time(args[0])
+  test_is_timestring(args[0])
+  test_is_timestring('3h51')
+  test_is_timestring('3h')
+  test_is_timestring('05')
+  test_is_timestring('05ee')
+  test_is_timestring('text')
   print(t)
